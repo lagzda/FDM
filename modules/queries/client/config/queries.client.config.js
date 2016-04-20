6eceb1d8476de8c20ecb3b9a5732ff7a02e86852
lagzda <ralfs.lagzda@gmail.com>
2016-04-20 08:14:16 +0100

    removed unnecessary items

--- modules/queries/client/config/queries.client.config.js
+++ modules/queries/client/config/queries.client.config.js
@@ -3,11 +3,6 @@
 // Configuring the Articles module
 angular.module('queries').run(['Menus',
   function (Menus) {
-    // Add the articles dropdown item
-    Menus.addMenuItem('topbar', {
-      title: 'Available queries',
-      state: 'queries.list',
-      roles: ['user']
-    }); 
+    
   }
 ]);
